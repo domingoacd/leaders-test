65cac327c9ea0cc32f47ff78a02b479a51117b11
Domingo Cabeza <domingoacd1@gmail.com>
2021-02-03 16:16:58 -0400

    Added some func

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+const showSideBar = (e) => {
+  const sideBar = document.getElementById('sidebar');
+  const overlay = document.querySelector('.j-overlay')
+
+  sideBar.classList.add('show')
+  overlay.classList.add('show')
+}
+
+const attachSideProfileEvent = () => {
+  const showSideBarBtn = document.querySelector('.j-open-side-profile');
+
+  showSideBarBtn?.addEventListener('click', showSideBar);
+}
+
+const startTestFunctionalities = () => {
+  attachSideProfileEvent()
+}
+window.addEventListener('load', startTestFunctionalities);
