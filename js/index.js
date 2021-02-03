d53e77dc0df1a7e066a0e1989e83bdf4900e81d2
Domingo Cabeza <domingoacd1@gmail.com>
2021-02-03 16:55:49 -0400

    change menu

--- js/index.js
+++ js/index.js
@@ -10,10 +10,22 @@ const showSideBar = (e) => {
   document.body.classList.add('showing-overlay');
 }
 
+const hideSideBar = (e) => {
+  const sideBar = document.getElementById('sidebar');
+  const overlay = document.querySelector('.j-overlay')
+
+  sideBar.classList.remove('show')
+  overlay.classList.remove('show')
+
+  document.body.classList.remove('showing-overlay');
+}
+
 const attachSideProfileEvent = () => {
   const showSideBarBtn = document.querySelector('.j-open-side-profile');
+  const hideSideBarBtn = document.querySelector('.j-close-side-profile');
 
   showSideBarBtn?.addEventListener('click', showSideBar);
+  hideSideBarBtn?.addEventListener('click', hideSideBar);
 }
 
 const startTestFunctionalities = () => {
